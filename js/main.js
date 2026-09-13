2cc4dfd0345c6bb80dc8be868e6f5c25f4c88023
Yash Kumar <yashkumar.connect@gmail.com>
2026-09-13 15:26:47 +0530

    adding smooth scrolling

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
   renderProfile("profileContainer");
   renderProjects("projectsContainer");
 });
+
+const backToTop = document.querySelector(".back-to-top");
+
+backToTop.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
